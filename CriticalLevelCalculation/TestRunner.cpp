ea69f7d4c874db271572cc6ab181edad54a0276c
Alex Gavrishev <alex.gavrishev@gmail.com>
2012-10-19 23:10:58 +0200

    add 2 more tests

--- CriticalLevelCalculation/TestRunner.cpp
+++ CriticalLevelCalculation/TestRunner.cpp
@@ -17,7 +17,7 @@ void TestRunner::testInvolvedCellsSeeker() {
 	assertVectorsSame(actual1,expected1);
 	std::cout << "Ok" << std::endl;
 
-	std::cout << " -- 2: ";
+	std::cout << " -- 2: Right ";
 	Point expectedArr2[] = { Point(4,4), Point(5,4) };
 	std::vector<Point> expected2 (expectedArr2, expectedArr2 + sizeof(expectedArr2) / sizeof(expectedArr2[0]) );
 	std::vector<Point> actual2 = seeker.seek(Point(3,4), Point(6,4));
@@ -25,7 +25,7 @@ void TestRunner::testInvolvedCellsSeeker() {
 	assertVectorsSame(actual2,expected2);
 	std::cout << "Ok" << std::endl;
 
-	std::cout << " -- 3: ";
+	std::cout << " -- 3: Up,Left ";
 	Point expectedArr3[] = { Point(4,4), Point(4,3), Point(5,3) };
 	std::vector<Point> expected3 (expectedArr3, expectedArr3 + sizeof(expectedArr3) / sizeof(expectedArr3[0]) );
 	std::vector<Point> actual3 = seeker.seek(Point(6,3), Point(3,4));
@@ -33,7 +33,7 @@ void TestRunner::testInvolvedCellsSeeker() {
 	assertVectorsSame(actual3,expected3);
 	std::cout << "Ok" << std::endl;
 
-	std::cout << " -- 4: ";
+	std::cout << " -- 4: Left,Up ";
 	Point expectedArr4[] = { Point(6,4), Point(6,5), Point(7,5) };
 	std::vector<Point> expected4 (expectedArr4, expectedArr4 + sizeof(expectedArr4) / sizeof(expectedArr4[0]) );
 	std::vector<Point> actual4 = seeker.seek(Point(7,6), Point(6,3));
@@ -41,6 +41,21 @@ void TestRunner::testInvolvedCellsSeeker() {
 	assertVectorsSame(actual4,expected4);
 	std::cout << "Ok" << std::endl;
 
+	std::cout << " -- 5: Up ";
+	Point expectedArr5[] = { Point(7,4), Point(7,5) };
+	std::vector<Point> expected5 (expectedArr5, expectedArr5 + sizeof(expectedArr5) / sizeof(expectedArr5[0]) );
+	std::vector<Point> actual5 = seeker.seek(Point(7,6), Point(7,3));
+
+	assertVectorsSame(actual5,expected5);
+	std::cout << "Ok" << std::endl;
+
+	std::cout << " -- 6: Long Distance ";
+	Point expectedArr6[] = { Point(2,9), Point(2,8), Point(3,8), Point(3,7), Point(4,7), Point(4,6), Point(5,6), Point(6,6), Point(6,5), Point(7,5), Point(7,4), Point(8,4), Point(8,3), Point(9,3), Point(10,3), Point(10,2), Point(11,2), Point(11,1) };
+	std::vector<Point> expected6 (expectedArr6, expectedArr6 + sizeof(expectedArr6) / sizeof(expectedArr6[0]) );
+	std::vector<Point> actual6 = seeker.seek(Point(12,1), Point(1,9));
+
+	assertVectorsSame(actual6,expected6);
+	std::cout << "Ok" << std::endl;
 }
 
 template <class T>
